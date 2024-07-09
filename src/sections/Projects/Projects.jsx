1104753e8435255f08453564fb2de89a994447b0
lesediS <selekelesedi25@gmail.com>
2024-07-09 11:03:26 +0200

    Added some projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,19 +1,52 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
-import MusicPlayer from '../../assets/musicplayer.png'
+import MusicPlayer from "../../assets/musicplayer.png";
+import News from '../../assets/news.png'
+import TicTacToe from '../../assets/tictactoe.png'
+import Chat from '../../assets/chatapp.png';
+import CMS from '../../assets/Geeks4Learning-Logo-with-Slogan.png'
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={MusicPlayer}
+          link="https://github.com/lesediS/musicplayer"
+          h3="Music Player"
+          p="Music app with Jetpack Compose"
+        />
 
-        <a href="https://github.com/lesediS/musicplayer" target="_blank"  c>
-          <img className="hover" src={MusicPlayer} alt="Music Player lgo"/>
-          <h3>Music Player</h3>
-          <p>Music streaming app with JetPack Compose</p>
-        </a>
-        
+        <ProjectCard
+          src={News}
+          link="https://github.com/lesediS/news-app"
+          h3="News App"
+          p="News app with Jetpack Compose"
+        />
+
+        <ProjectCard
+          src={Chat}
+          link="https://github.com/lesediS/androidgeekschat"
+          h3="Chat App"
+          p="Chat app with Java"
+        />
+
+        <ProjectCard
+          src={TicTacToe}
+          link="https://github.com/lesediS/tictactoe"
+          h3="Tic Tac Toe - 3 In A Row"
+          p="Tic Tac Toe game with Jetpack Compose"
+        />
+
+        <ProjectCard
+          src={CMS}
+          link="https://github.com/lesediS/cms"
+          h3="Content Management System"
+          p="Content management system for learnership with React and Spring Boot"
+        />
+
       </div>
     </section>
   );
