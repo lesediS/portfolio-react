90dff1782f52b6aedd72dd409f52380ceab71dc8
lesediS <selekelesedi25@gmail.com>
2024-07-09 09:03:45 +0200

    Added first project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./ProjectsStyles.module.css";
+import MusicPlayer from '../../assets/musicplayer.png'
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+
+        <a href="https://github.com/lesediS/musicplayer" target="_blank"  c>
+          <img className="hover" src={MusicPlayer} alt="Music Player lgo"/>
+          <h3>Music Player</h3>
+          <p>Music streaming app with JetPack Compose</p>
+        </a>
+        
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
