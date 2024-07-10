b73e84b805e544cf1d9635a33431ea45fff0dba2
lesediS <selekelesedi25@gmail.com>
2024-07-10 11:27:34 +0200

    Added footer, need to add copyrights

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./FooterStyles.module.css";
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>&#169; 2024 COPYRIGHT INFO</p>
+    </section>
+  );
+}
+
+export default Footer;
