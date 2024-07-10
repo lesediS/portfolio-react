d474dcb1620696c7823d334b23fbfee9a9649c46
lesediS <selekelesedi25@gmail.com>
2024-07-10 14:57:30 +0200

    copy right

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./FooterStyles.module.css";
 function Footer() {
   return (
     <section id="footer" className={styles.container}>
-      <p>&#169; 2024 COPYRIGHT INFO</p>
+      <p>&#169; 2024 LESEDI SELEKE. <br/> All rights reserved.</p>
     </section>
   );
 }
