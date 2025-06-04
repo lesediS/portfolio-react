58ddd51906c33ca8b7ef7a89cfe6324e23c4d930
Lesedi Seleke <106717766+lesediS@users.noreply.github.com>
2025-06-04 07:52:17 +0200

    Added security for project cards

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target="_blank">
+    <a href={link}
+        target="_blank"
+        rel="noopener noreferrer">
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
